3e3f12e3bef463399b91e09a0896d32b13757f6e
JumaboyevQuvondiq <jumaboyevquvondiqm5202@gmail.com>
2023-02-05 17:41:00 +0500

    create Profile page for  admin Panel

--- src/TwistFood.Service/Interfaces/Admins/IAdminService.cs
+++ src/TwistFood.Service/Interfaces/Admins/IAdminService.cs
@@ -1,5 +1,6 @@
 ﻿using TwistFood.Service.Dtos.AccountAdmin;
 using TwistFood.Service.Dtos.Accounts;
+using TwistFood.Service.ViewModels.Accounts;
 
 namespace TwistFood.Service.Interfaces.Admins
 {
@@ -7,5 +8,7 @@ namespace TwistFood.Service.Interfaces.Admins
     {
         public Task<string> AdminLoginAsync(AdminLoginDto adminLoginDto);
         public Task<bool> AdminRegisterAsync(AdminRegisterDto adminRegisterDto);
+        public Task<AdminViewModel> GetAsync(long Id);
+        public Task<bool> AdminUpdateAsync();
     }
 }
---
+++ src/TwistFood.Service/ViewModels/Accounts/AdminViewModel.cs
@@ -0,0 +1,20 @@
+﻿using OnlineMarket.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwistFood.Service.ViewModels.Accounts
+{
+    public class AdminViewModel : BaseEntity
+    {
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string? ImagePath { get; set; }
+        public string BirthDate { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = String.Empty;
+        public string PassportSeriaNumber { get; set; } = String.Empty;
+        public string Email { get; set; }   = string.Empty;
+    }
+}
