7eb70f4b2011010ff4f25f5bc7921422849e75d8
JumaboyevQuvondiq <jumaboyevquvondiqm5202@gmail.com>
2023-02-06 12:11:36 +0500

    add method delete and update for users (Admin panel)

--- src/TwistFood.Service/Dtos/Accounts/AccountUpdateDto.cs
+++ src/TwistFood.Service/Dtos/Accounts/AccountUpdateDto.cs
@@ -1,7 +1,12 @@
-﻿namespace TwistFood.Service.Dtos.Accounts
+﻿using OnlineMarket.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace TwistFood.Service.Dtos.Accounts
 {
-    public class AccountUpdateDto
+    public class AccountUpdateDto 
     {
+        public long? Id { get; set; }
+        [MaxLength(70),MinLength(2)]
         public string? FullName { get; set; }
     }
 }
